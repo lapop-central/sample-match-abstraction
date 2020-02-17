f4550880b705795e33830d110c1a98c162a438c1
Maita Schade <maita.schade@vanderbilt.edu>
2020-02-17 08:51:05 -0600

    added region_combine

--- src/region_combine.r
+++ src/region_combine.r
@@ -4,10 +4,90 @@ rm(list = ls(all=T))
 # necessary packages
 library(data.table)
 
+#files
+prepdir <- "C:/Users/schadem/Box Sync/LAPOP Shared/working documents/maita/Coordination/IDB Online Trust/prep/out/geo/"
+outdir <- "C:/Users/schadem/Box Sync/LAPOP Shared/working documents/maita/Coordination/IDB Online Trust/doc/design/geo/"
+
+# AR
 # load IPUMS with regions--manually created in QGIS
-IPUMS_regions <- fread("C:/Users/schadem/Box Sync/LAPOP Shared/working documents/maita/Coordination/IDB Online Trust/prep/out/geo/PE_geo2_regions_crosswalk.csv",encoding="UTF-8")
+IPUMS_regions <- fread(paste0(
+  prepdir,"AR_geo2_regions_crosswalk.csv"),
+  encoding="UTF-8")
 # load IPUMS with Netquest--out of Netquest processing
-IPUMS_Netquest <- fread("C:/Users/schadem/Box Sync/LAPOP Shared/working documents/maita/Coordination/IDB Online Trust/prep/out/geo/PE_geo2_codebook.csv",encoding = 'UTF-8')
+IPUMS_Netquest <- fread(paste0(
+  prepdir,"AR_geo2_codebook.csv"),
+  encoding = 'UTF-8')
+regions_table <- IPUMS_Netquest[IPUMS_regions,.(REGION,ADMIN_NAME,IPUM2010,geo2_code),on="IPUMS_geo2_code==IPUM2010"]
+# write out the regions table
+write.csv(regions_table,
+          paste0(outdir,"AR_regions.csv"))
+
+# BR
+# load IPUMS with regions--manually created in QGIS
+IPUMS_regions <- fread(paste0(
+  prepdir,"BR_geo1_regions_crosswalk.csv"),
+  encoding="UTF-8")
+# load IPUMS with Netquest--out of Netquest processing
+IPUMS_Netquest <- fread(paste0(
+  prepdir,"BR_geo1_codebook.csv"),
+  encoding = 'UTF-8')
+regions_table <- IPUMS_Netquest[IPUMS_regions,.(REGION,ADMIN_NAME,IPUM2010,geo1_code),on="IPUMS_geo1_code==IPUM2010"]
+# write out the regions table
+write.csv(regions_table,
+          paste0(outdir,"BR_regions.csv"))
+
+# CL
+# load IPUMS with regions--manually created in QGIS
+IPUMS_regions <- fread(paste0(
+  prepdir,"CL_geo1_regions_crosswalk.csv"),
+  encoding="UTF-8")
+# load IPUMS with Netquest--out of Netquest processing
+IPUMS_Netquest <- fread(paste0(
+  prepdir,"CL_geo1_codebook.csv"),
+  encoding = 'UTF-8')
+regions_table <- IPUMS_Netquest[IPUMS_regions,.(REGION,ADMIN_NAME,IPUM2002,geo1_code),on="IPUMS_geo1_code==IPUM2002"]
+# write out the regions table
+write.csv(regions_table,
+          paste0(outdir,"CL_regions.csv"))
+
+# CO
+# load IPUMS with regions--manually created in QGIS
+IPUMS_regions <- fread(paste0(
+  prepdir,"CO_geo1_regions_crosswalk.csv"),
+  encoding="UTF-8")
+# load IPUMS with Netquest--out of Netquest processing
+IPUMS_Netquest <- fread(paste0(
+  prepdir,"CO_geo1_codebook.csv"),
+  encoding = 'UTF-8')
+regions_table <- IPUMS_Netquest[IPUMS_regions,.(REGION,ADMIN_NAME,IPUM2005,geo1_code),on="IPUMS_geo1_code==IPUM2005"]
+# write out the regions table
+write.csv(regions_table,
+          paste0(outdir,"CO_regions.csv"))
+
+# MX
+# load IPUMS with regions--manually created in QGIS
+IPUMS_regions <- fread(paste0(
+  prepdir,"MX_geo1_regions_crosswalk.csv"),
+  encoding="UTF-8")
+# load IPUMS with Netquest--out of Netquest processing
+IPUMS_Netquest <- fread(paste0(
+  prepdir,"MX_geo1_codebook.csv"),
+  encoding = 'UTF-8')
+regions_table <- IPUMS_Netquest[IPUMS_regions,.(REGION,ADMIN_NAME,IPUM2015,geo1_code),on="IPUMS_geo1_code==IPUM2015"]
+# write out the regions table
+write.csv(regions_table,
+          paste0(outdir,"MX_regions.csv"))
+
+# PE
+# load IPUMS with regions--manually created in QGIS
+IPUMS_regions <- fread(paste0(
+  prepdir,"PE_geo2_regions_crosswalk.csv"),
+  encoding="UTF-8")
+# load IPUMS with Netquest--out of Netquest processing
+IPUMS_Netquest <- fread(paste0(
+  prepdir,"PE_geo2_codebook.csv"),
+  encoding = 'UTF-8')
 regions_table <- IPUMS_Netquest[IPUMS_regions,.(REGION,ADMIN_NAME,IPUM2007,geo2_code),on="IPUMS_geo2_code==IPUM2007"]
 # write out the regions table
-write.xlsx(regions_table,"C:/Users/schadem/Box Sync/LAPOP Shared/working documents/maita/Coordination/IDB Online Trust/doc/design/geo/PE_regions.xlsx")
+write.csv(regions_table,
+          paste0(outdir,"PE_regions.csv"))
