44c1feccff7ce4b6dfe05f39ce9a6872231f81d0
Maita Schade <maita.schade@vanderbilt.edu>
2020-09-28 11:09:00 +0200

    updated and documented 2_process-geo.py, cleaned up some other files

--- src/2_process-geo.py
+++ src/2_process-geo.py
@@ -1,63 +1,68 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Loading packages
+# Creating geo-structures that match across IPUMS and Netquest
 
 # In[263]:
 
-
+# importing packages and setting some settings
 import pandas as pd
 pd.options.display.width = 0 #setting flexible print output width
 pd.set_option('display.max_columns', 500)
 from fuzzywuzzy import fuzz, process
-#import re
 import numpy as np
 
 
 
-# Defining specific parameters
 # In[1]:
-params = pd.read_csv("./country_parameters1.csv")
+# Defining specific parameters for each country.
+# Good to make sure these are correct each time you get a new extract
+params = pd.read_csv("./country_parameters.csv")
 params
 
 # In[265]:
 
 
-for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
+for country in ['AR']:#'BR',"CO",'MX',"PE",'CL',
     print("working on ", country)
 
-
-# In[271]:
-
-    
+    # Here, you set the file containing the IPUMS data for each country
+    # We should consider putting this in the country_parameters spread-
+    # sheet, since it is used again in other places.
     if country == "AR":
-        ipumsfile = "../../raw/ipums/"+country+"/ipumsi_00015.csv"
+        ipumsfile = "../raw/ipums/"+country+"/ipumsi_00015.csv"
     
     elif country == "BR":
-        ipumsfile = "../../raw/ipums/"+country+"/ipumsi_00026.csv"
+        ipumsfile = "../raw/ipums/"+country+"/ipumsi_00036.csv"
         
     elif country == "CL":
-        ipumsfile = "../../raw/ipums/"+country+"/ipumsi_00020.csv"
+        ipumsfile = "../ipums/"+country+"/ipumsi_00020.csv"
         
     elif country=="CO":
-        ipumsfile = "../../raw/ipums/"+country+"/ipumsi_00022.csv"
+        ipumsfile = "../raw/ipums/"+country+"/ipumsi_00022.csv"
         
     elif country=="MX":
-        ipumsfile = "../../raw/ipums/"+country+"/ipumsi_00021.csv"
+        ipumsfile = "../raw/ipums/"+country+"/ipumsi_00021.csv"
         
     elif country=="PE":
-        ipumsfile = "../../raw/ipums/"+country+"/ipumsi_00032.csv"
+        ipumsfile = "../raw/ipums/"+country+"/ipumsi_00032.csv"
       
+    # Set all the other parameters, based on the sheet and other things
+    # we know.
     year = str(params[params.country==country].year.values[0])
     geo1_nq = params[params.country==country].geo1_nq.values[0]
     geo2_nq = params[params.country==country].geo2_nq.values[0]
     geo1_ipums = "GEO1_"+ country + year
     geo2_ipums = "GEO2_"+ country + year
     
+    # These are output files, relative to the location of this script. 
+    # Requires setting up the file structure beforehand!
     netquestfile = "../out/panel_country/"+country+"_netquest-panel.csv"
     dictfile = "../out/panel_country/"+country+"_levels.xlsx"
+    # These need to be saved from the IPUMS codebooks--mapping between code and name for each geography
     geo2file = '../out/ipums_country/ipums_codebook_'+geo2_ipums+'.csv'
     geo1file = '../out/ipums_country/ipums_codebook_'+geo1_ipums+'.csv'
+    # This needs to be created in a GIS based on IPUMS shapefiles
     centroidfile = '../out/geo/'+country+'_geo_centroids.csv'
     panelout = '../out/panel_country/'+country+'_netquest-panel_geo.csv'
     ipumsout = '../out/ipums_country/'+country+'_ipums-census_geo.csv'
@@ -67,18 +72,12 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
     
     # Loading things
     
-    # In[272]:
-    
-    
-    netquestfile
-    
-    
-    # In[278]:
-    
     print("Reading in data...")
     census = pd.read_csv(ipumsfile)
     netquest = pd.read_csv(netquestfile)
     nq_dict = pd.read_excel(dictfile)
+    # The following accounts for some weirdness in individual countries' 
+    # input files.
     if country=="AR":
         # deal with mis-stored values in the current extract
         nq_dict.Valor = nq_dict.Valor.astype(str).str.replace(",00","").astype(int)
@@ -89,21 +88,13 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
         ipums_geo2 = pd.read_csv(geo2file,encoding='Latin1', names=['code','name'], skiprows=1)
         ipums_geo1 = pd.read_csv(geo1file,encoding='Latin1', names=['code','name'], skiprows=1)
     
-    
+    # This loads a file containing the centroids for the geographies of 
+    # interest. Somebody needs to create this in a GIS. 
+    # For an example of 
     geo2_centroids = pd.read_csv(centroidfile, encoding='latin1')
     
-    
-    # In[270]:
-    
-    
-    [k for k in netquest.columns if k.startswith('AR')]
-    
-    
-    # Defining functions
-    
-    # In[279]:
-    
-    
+# Defining functions
+        
     # one for fuzzy-joining dataframes
     def fuzzy_join(df1, df2, varname, subset=False, cutoff=90):
         '''Returns a dataframe the length of df1, with the matches on the given variable
@@ -169,23 +160,24 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
     
     
     # Setting up helper structures
-    
-    # In[280]:
     print("Setting up helper structures...")
     
+    # A dataframe for the netquest GEO1 codes
     nq_geo1 = nq_dict[nq_dict.Variable==geo1_nq]
     nq_geo1.columns = ["Variable",geo1_nq,geo1_nq+"_name"]
     nq_geo1 = nq_geo1[[geo1_nq,geo1_nq+"_name"]]
     
+    # dataframe for the netquest GEO2 codes
     nq_geo2 = nq_dict[nq_dict.Variable==geo2_nq]
     nq_geo2.columns = ["Variable",geo2_nq,geo2_nq+"_name"]
     nq_geo2 = nq_geo2[[geo2_nq,geo2_nq+"_name"]]
     
-    # setting up unique DF for geographies
+    # setting up unique DF for geographies from IPUMS
     ipums_geodf = census[[geo1_ipums,geo2_ipums]]
     ipums_geodf.columns = ['geo1_code','geo2_code']
     ipums_geodf.drop_duplicates(subset=['geo1_code','geo2_code'],inplace=True)
     
+    # merge geographies onto the code table for IPUMS
     ipums_geodf['geo1_name'] = ipums_geodf.merge(ipums_geo1, 
                                                  how='left', 
                                                  left_on = "geo1_code", 
@@ -199,16 +191,30 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
                                                  copy=False)['name'].values
     
     # Melting doubled-up geographies
-    ipums_geodf = ipums_geodf.geo2_name.str.split(',').apply(pd.Series)     .merge(ipums_geodf, right_index = True, left_index = True)     .drop(["geo2_name"], axis = 1)     .melt(id_vars = [k for k in ipums_geodf.columns if not (type(k)==int)|(k=='geo2_name')], value_name = "geo2_name")     .drop("variable", axis = 1)     .dropna(subset=['geo2_name'])
-    ipums_geodf.geo2_name = ipums_geodf.geo2_name.str.strip()
+    # first for geo2
+    ipums_geodf = ipums_geodf.geo2_name.str.split(',' # start with the geo2-names, broken up where there are multiple to a geography
+                                                ).apply(pd.Series
+                                                ).merge(ipums_geodf, right_index = True, left_index = True # merge back together with the whole set of geographies
+                                                ).drop(["geo2_name"], axis = 1
+                                                ).melt(id_vars = [k for k in ipums_geodf.columns if not (type(k)==int)|(k=='geo2_name')], value_name = "geo2_name"
+                                                ).drop("variable", axis = 1
+                                                ).dropna(subset=['geo2_name'])
+    ipums_geodf.geo2_name = ipums_geodf.geo2_name.str.strip() # remove whitespace
     
-    ipums_geodf = ipums_geodf.geo1_name.str.split(',').apply(pd.Series)     .merge(ipums_geodf, right_index = True, left_index = True)     .drop(["geo1_name"], axis = 1)     .melt(id_vars = [k for k in ipums_geodf.columns if not (type(k)==int)|(k=='geo1_name')], value_name = "geo1_name")     .drop("variable", axis = 1)     .dropna(subset=['geo1_name'])
+    # then the exact same thing for geo1
+    ipums_geodf = ipums_geodf.geo1_name.str.split(','
+                                                ).apply(pd.Series
+                                                ).merge(ipums_geodf, right_index = True, left_index = True
+                                                ).drop(["geo1_name"], axis = 1
+                                                ).melt(id_vars = [k for k in ipums_geodf.columns if not (type(k)==int)|(k=='geo1_name')], value_name = "geo1_name"
+                                                ).drop("variable", axis = 1
+                                                ).dropna(subset=['geo1_name'])
     ipums_geodf.geo1_name = ipums_geodf.geo1_name.str.strip()
     
     
-    # Country-specific pre-processing
+# Country-specific pre-processing--Argentina is a bit different because
+    # of the way CABA is represented
     
-    # In[284]:
     # Create the netquest geoframe
     
     if country=="AR":
@@ -238,18 +244,19 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
                      'geo1_name'] = 'CABA'
         nq_geodf = nq_geodf[(nq_geodf['geo1_code'].notna()) & (nq_geodf['geo2_code'].notna())] 
         nq_geodf[nq_geodf.geo1_code==1]
-    else:
-        nq_geodf = netquest.merge(nq_geo2, on=geo2_nq, how='left')                       .merge(nq_geo1, on=geo1_nq, how='left')[[geo1_nq,geo1_nq+'_name',geo2_nq,geo2_nq+'_name']]
+    else: # outside Argentina, everything is easy
+        # merge the geo-names onto the panel
+        nq_geodf = netquest.merge(nq_geo2, on=geo2_nq, how='left'
+                          ).merge(nq_geo1, on=geo1_nq, how='left'
+                          )[[geo1_nq,geo1_nq+'_name',geo2_nq,geo2_nq+'_name']]
+        # give it the proper names
         nq_geodf.columns = ['geo1_code','geo1_name','geo2_code','geo2_name']
-    
+        # get rid of duplicates
         nq_geodf.drop_duplicates(inplace=True)
     
     
-    # Country-independent pre-processing
-    
-    # In[285]:
-    
-    
+# Country-independent pre-processing
+        
     #Finding the duplicates in nq_geodf.geo2_code, and keeping the combinations that occur most often (which are quite clearly the good ones).
     nq_geodf['count'] = nq_geodf.apply(
         lambda r: sum((netquest[geo1_nq]==r['geo1_code'])&
@@ -261,15 +268,9 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
     nq_geodf.geo2_name = nq_geodf.geo2_name.str.title()
     
     
-    # In[286]:
-    
-    
-    nq_geodf
-    
-    
-    # More country-specific processing--manual fixes:
-    
-    # In[290]:
+# More country-specific processing--manual fixes:
+    # These need to be checked when you first run this,
+    # and you may need to add more depending on typos in the panel etc.
     
     print("Cleaning up names...")
     if country=="AR":
@@ -286,16 +287,21 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
                         'geo2_name'] = "Chascomús"
         ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Jose C. Paz")),
                         'geo2_name'] = "José C. Paz"
-        nq_geodf.loc[(nq_geodf.geo2_name.str.contains("Paso De Indios"))                    &(nq_geodf.geo1_name=="Chubut"),
+        nq_geodf.loc[(nq_geodf.geo2_name.str.contains("Paso De Indios")
+                      )&(nq_geodf.geo1_name=="Chubut"),
                         'geo2_name'] = "Paso de los Indios"
-        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Coronel de Marine L. Rosales"))&(ipums_geodf.geo1_name=="Buenos Aires"),
+        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Coronel de Marine L. Rosales")
+                         )&(ipums_geodf.geo1_name=="Buenos Aires"),
                         'geo2_name'] = "Coronel De Marina Leonardo Rosales"
-        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Veinticinco de Mayo"))                    &(ipums_geodf.geo1_name=="Buenos Aires"),
+        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Veinticinco de Mayo")
+                         )&(ipums_geodf.geo1_name=="Buenos Aires"),
                         'geo2_name'] = "25 de Mayo"
-        nq_geodf.loc[(nq_geodf.geo2_name.str.contains("Pueyrredón"))                    &(nq_geodf.geo1_name=="Buenos Aires"),
+        nq_geodf.loc[(nq_geodf.geo2_name.str.contains("Pueyrredón")
+                      )&(nq_geodf.geo1_name=="Buenos Aires"),
                         'geo2_name'] = "General Pueyrredón"
                         
-        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("La Capital"))                    &(ipums_geodf.geo1_name=="San Luis"),
+        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("La Capital")
+                         )&(ipums_geodf.geo1_name=="San Luis"),
                         'geo2_name'] = "Juan Martín de Pueyrredón"
                         # La Capital renamed to JMdP in 2010
 #        ipums_geodf.loc[(ipums_geodf.geo2_name=="Maipú")                    &(ipums_geodf.geo2_code==6050),
@@ -312,11 +318,13 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
         ipums_geodf.loc[ipums_geodf.geo1_name.str.contains("Federal District"),
                     'geo1_name'] = "Distrito Federal"
     
-        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Santarém"))& (ipums_geodf.geo1_name.str.contains("Paraíba")),
+        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Santarém")
+                         )&(ipums_geodf.geo1_name.str.contains("Paraíba")),
                         'geo2_name'] = "Joca Claudino"
                         # name change in 2010; using new name here
     
-        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Presidente Juscelino"))&                     (ipums_geodf.geo1_name.str.contains("Rio Grande do Norte")),
+        ipums_geodf.loc[(ipums_geodf.geo2_name.str.contains("Presidente Juscelino")
+                         )&(ipums_geodf.geo1_name.str.contains("Rio Grande do Norte")),
                         'geo2_name'] = "Serra Caiada"
                         # name change; using new name here        
     
@@ -407,12 +415,9 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
                         'geo2_name'] = "Maynas"
     
     
-    # Actual computing
+# Actual computing
     
-    # In[291]:
-    
-    
-    # The fuzzy join
+    # The fuzzy join on names of geographies
     print("Finding matching geographies...")
     nq_geodf[['geo1_match_name','geo1_match_score','geo1_match_index']] = fuzzy_join(nq_geodf, ipums_geodf, 'geo1_name')[['name','score','index']]
     nq_geodf[['geo2_match_name','geo2_match_score','geo2_match_index']] = fuzzy_join(nq_geodf, ipums_geodf, 'geo2_name', 
@@ -430,7 +435,8 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
         )==0
     )      
         
-    # determination of cutoff
+    # determination of cutoff -- depending on the number you set here, it'll
+    # show you matches that are worse than that cut off
     print("Showing problematic geographies...")
     print(
           nq_geodf[(nq_geodf.geo2_match_score<80)][['geo1_name',
@@ -442,21 +448,24 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
                                             'geo2_match_score'
                                            ]
                     ].sort_values('geo1_name'))
+    # now, getting command-line input to determine which matches to actually
+    # throw out--you want to get rid of ones that are NA or similar
     cutoff = input(country + ": Decide at what level to cut off matches: ")
     
     has_ipums_geo = nq_geodf.geo2_match_score>int(cutoff )
     
-    # attaching geography codes
+    # attaching geography codes to the panel
     nq_geodf['IPUMS_geo1_code'] = np.nan
     nq_geodf['IPUMS_geo2_code'] = np.nan
     
-    nq_geodf.loc[has_ipums_geo,'IPUMS_geo2_code'] = nq_geodf[has_ipums_geo]                            .geo2_match_index                            .astype(int)                            .apply(
-                                    lambda i: ipums_geodf.loc[i,'geo2_code']
-                                .astype(int)
+    nq_geodf.loc[has_ipums_geo,'IPUMS_geo2_code'] = nq_geodf[has_ipums_geo
+                                                    ].geo2_match_index.astype(int).apply(
+                                    lambda i: ipums_geodf.loc[i,'geo2_code'].astype(int)
     )
-    nq_geodf.loc[has_ipums_geo,'IPUMS_geo1_code'] = nq_geodf[has_ipums_geo]                            .geo1_match_index                            .astype(int)                            .apply(
-                                    lambda i: ipums_geodf.loc[i,'geo1_code']
-                                .astype(int)
+                                                        
+    nq_geodf.loc[has_ipums_geo,'IPUMS_geo1_code'] = nq_geodf[has_ipums_geo
+                                                    ].geo1_match_index.astype(int).apply(
+                                    lambda i: ipums_geodf.loc[i,'geo1_code'].astype(int)
     )
                                 
     # Merge centroids onto NQ geometries:
@@ -465,13 +474,13 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
                    right_on="IPUM"+year,
                    how='left'
                   ).drop('IPUM'+year, axis=1)
-    #Merge NQ geometries onto NQ data:
+    # Merge NQ geometries onto NQ data:
     panel_geo = netquest.merge(nq_geodf_merged[['X','Y','geo2_code']],
                    left_on=geo2_nq,
                    right_on='geo2_code',
                    how='left'
                   )
-    #Merge census geometries onto census data
+    # Merge census geometries onto census data
     census_geo = census.merge(geo2_centroids[['ADMIN_NAME','X',"Y",'IPUM'+year]],
                               left_on = geo2_ipums,
                               right_on='IPUM'+year,
@@ -481,7 +490,6 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
     
     # Writing out
     
-    # In[]:
     print("Saving the geo codebook...")
     
     codebook_geo2 = nq_geodf[["geo1_name","geo1_code","geo2_name","geo2_code","geo1_match_name","IPUMS_geo1_code","geo2_match_name","IPUMS_geo2_code"]]
@@ -489,18 +497,16 @@ for country in ['BR']:#,"CO",'MX',"PE",'CL','AR'
     
     codebook_geo1.to_csv(codebook1out)    
     codebook_geo2.to_csv(codebook2out)
-    # In[292]:
+    
     print("Writing out the result...")
     
-    if panel_geo.shape[0]!=netquest.shape[0]:
+    if panel_geo.shape[0]!=netquest.shape[0]: #If we don't have the same number of netquest geometries we started with
         print("Problem with panel shape match")
-    if census_geo.shape[0]!=census.shape[0]:
+    if census_geo.shape[0]!=census.shape[0]: #If we don't have the same number of census geometries we started with
         print("Problem with census shape match")
     panel_geo.loc[:,[not("Unnamed" in k) for k in panel_geo.columns]].to_csv(panelout)
     census_geo.loc[:,[not("Unnamed" in k) for k in census_geo.columns]].to_csv(ipumsout)
 
-
-# In[ ]:
 
 
 
