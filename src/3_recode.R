17c27bd9fe0768686334705978db04eb3aee04e9
Maita Schade <maita.schade@vanderbilt.edu>
2020-09-28 11:47:19 +0200

    updated paths and documented 3_recode.R

--- src/3_recode.R
+++ src/3_recode.R
@@ -7,7 +7,7 @@
 
 # This notebook helps in recoding panel and census datasets to a compatible format.
 # Important input that need to be adjusted with country specifics:
-#   * recode_Netquest_IPUMS_COUNTRY.csv currently supplies matching variables
+#   * matching-vars.csv currently supplies matching variables
 #   * country_parameters.csv supplies parameters (such as specific labels or strata)
 #   * recode_country.R contains the individual recodes for each country
 
@@ -18,13 +18,14 @@ rm(list = ls(all = TRUE))
 library(data.table)
 library(bit64)
 # set working dir
-setwd('C:/Users/schadem/Box/LAPOP Shared/2_Projects/2020 IDB Trust/prep/src/')
+setwd('C:/Users/schadem/Box/LAPOP Shared/2_Projects/2019 APES/Matching process/src/')
 
 # Setting specifics--iterate over countries.
 
-for (country in c("BR")){ # "CL","AR","CO","MX","PE"
+for (country in c("AR")){ # "CL","BR","CO","MX","PE"
 
- # country<-"PE"
+# For testing/dev, just set one country
+country<-"AR"
 # Defining files--make sure the dirs are okay; other than that you shouldn't need to touch this if the file structure is set up properly.
 
 datadir <-  paste0('../out/')
@@ -33,9 +34,9 @@ censuspath <- paste0(datadir,'ipums_country/',country,'_ipums-census_geo.csv')
 
 netquestpath <- paste0(datadir, "panel_country/", country, "_netquest-panel_geo.csv")
 
-varpath <- paste0("../../doc/matching/matching-vars.csv") #table specifying matching vars by country
-parampath <- "./country_parameters1.csv" # set specific parameters in this file
-regiopath <- paste0("../../doc/design/geo/",country,"_regions.csv")
+varpath <- "./matching_vars.csv" #table specifying matching vars by country
+parampath <- "./country_parameters.csv" # set specific parameters in this file
+regiopath <- paste0("../doc/design/geo/",country,"_regions.csv")
 
 # Load external function to do the dirty work, depending on country etc.
 # Note that this function will need to get updated when adding new countries/data sources.
@@ -43,14 +44,14 @@ source("./recode_country.R")
 
 #Load country-specific parameters:
 params <- fread(parampath,key = "country")[country,]
-NQ_id <- params$NQ_id
-strats <- unlist(Filter(f = Negate(is.na), params[,c("strat1","strat2")]))
+NQ_id <- params$NQ_id #netquest ID label used in panel extract
+strats <- unlist(Filter(f = Negate(is.na), params[,c("strat1","strat2")])) #strata variables
 
 # We have to recode the various characteristics we have at our disposal.
 
 matching.vars <- Filter(
   f=Negate(is.na),
-  fread("../../doc/matching/matching_vars.csv",na.strings = "")[[country]]
+  fread(varpath,na.strings = "")[[country]]
 )
 cat("Matching on: ")
 cat(matching.vars)
@@ -58,14 +59,15 @@ cat(matching.vars)
 
 ## Recode IPUMS
 
-# Load datafile
+# Load IPUMS datafile
 print(censuspath)
 census<- fread(censuspath)
 
 # Recode
 census.proc <- countryRecode(dt = census, source = 'ipums', country = country)
-# Summarize
-# summary(census.proc)
+# give warning of variables that have a lot of NA in the end--records
+# with NA in matching vars are dropped, so if some variable excludes 
+# a lot of records we need to remove that variable from matching
 cat("Census vars ending up with lots of NA: ")
 cat(Filter(function(name){sum(is.na(census.proc[[name]]))}/nrow(census.proc) > 0.2, names(census.proc)))
 
@@ -77,7 +79,7 @@ cat(Filter(function(name){sum(is.na(census.proc[[name]]))}/nrow(census.proc) > 0
 # nrow(census.proc[PE2007A_CABLETV==1,cable:=1])
 # This is special ed, and not in universe.
 
-# Add the region
+# Add the region and capital flag
 regio_dict <- fread(regiopath)
 ## Figure out what we are lining up
 codevars <- list(IPUMS=grep("IPUM",names(regio_dict),value=T),
@@ -95,7 +97,7 @@ census.proc[,c("region","capital"):=regio_dict[census.proc,
 ### test a little bit
 # unique(census.proc[region==3,.(ADMIN_NAME)])
 
-# Give ID to all members
+# Give ID to all members, as combination of household and person ID
 nSERIAL <- max(nchar(as.character(census.proc$SERIAL)))
 nPERN <- max(nchar(as.character(census.proc$PERNUM)))
 # it's twice as fast to pull the calculation of the width of these out front
@@ -110,18 +112,16 @@ census.proc[,censusId:=paste0(
 
 cat("\ncensusId is unique: ")
 cat(max(table(census.proc$censusId)) == 1) #make sure is unique!
+# if this is FALSE, asses what is going on with these supposedly unique IDs
 
 # Then cut things down                         
 census.proc <- census.proc[census.proc$age>17,]
 census.proc <- census.proc[,c('censusId','PERWT',matching.vars,strats[strats!=""]),with=F]
 census.proc <- na.omit(census.proc)
-#head(census.proc)
-
-# summary(census.proc)
 
 # what proportion ends up in sampling frame
 cat("Proportion of census in sampling frame: ")
-cat(dim(census.proc)/dim(census))
+cat((dim(census.proc)/dim(census))[1])
 
 # Save to disk:
 fwrite(x = census.proc, 
@@ -134,15 +134,13 @@ fwrite(x = census.proc,
 
 # Load datafile
 netquest <- fread(netquestpath)
-# sort(unique(netquest$X))
 
 # Recode
 netquest.proc <- countryRecode(dt = netquest, source = 'netquest', country = country)
 
 cat("Netquest vars ending with lots of NA: ")
 cat(Filter(function(name){sum(is.na(netquest.proc[[name]]))}/nrow(netquest.proc) > 0.1, names(netquest.proc)))
-
-dim(netquest.proc)/dim(netquest)
+# If any of the matching variables have a lot of NA, investigate and consider dropping that one
 
 # Add the region
 regio_dict <- fread(regiopath)
@@ -172,7 +170,7 @@ netquest.proc <- na.omit(netquest.proc)
 
 # what proportion ends up in sampling frame
 cat("\n\nProportion of Netquest available: ")
-cat(dim(netquest.proc)/dim(netquest))
+cat((dim(netquest.proc)/dim(netquest))[1])
 
 # Save to disk:
 fwrite(x = netquest.proc, 
