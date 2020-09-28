a718fb7b744ff0b3e48b01fc1464d67c27d150df
Maita Schade <maita.schade@vanderbilt.edu>
2020-09-28 11:08:06 +0200

    Cleaned up and documented 1_process-Netquest.py; cleaned up some other files

--- src/1_process-Netquest.py
+++ src/1_process-Netquest.py
@@ -26,14 +26,16 @@ paneldate = "200612"
 # In[3]:
 
 
-outdir = "../out/panel_country/"
+outdir = ("C:/Users/schadem/Box/LAPOP Shared/2_Projects/"+
+            "2019 APES/Matching process/out/panel_country/")
 
 
 # In[4]:
-
+## This defines the files we're using from Netquest. 
+## They occasionally change their format/naming convention
 
 paneldir = ("C:/Users/schadem/Box/LAPOP Shared/2_Projects/"+
-            "2020 IDB Trust/raw/netquest/"+paneldate+"/")
+            "2019 APES/Matching process/raw/netquest/"+paneldate+"/")
 panelfile = [s for s in os.listdir(paneldir) if s.endswith(".csv")][0]
 manualfile = "Manual2.xlsx"
 print("loading panel data: ", panelfile)
@@ -42,7 +44,8 @@ print("loading panel data: ", panelfile)
 # ### this can just do its thing
 
 # In[5]:
-
+## Here, we read in the most recent panelfile.
+## If there are issues, try changing the encoding.
 
 panels = pd.read_csv(paneldir+panelfile,
                      sep=';', 
@@ -55,16 +58,15 @@ panels = pd.read_csv(paneldir+panelfile,
 
 
 # In[ ]:
+## Check if certain columns are there, specifically those used later on
+
+print([k for k in panels.columns if "municipio" in k])
+print([k for k in panels.columns if "COUNTRY" in k])
 
 
-[k for k in panels.columns if "municipio" in k]
-#[k for k in panels.columns if "COUNTRY" in k]
-
-
-# Creating a dictionary of individual country dataframes, and cleaning them up--pick out non-empty variables for each country.
 
 # In[ ]:
-
+# Creating a dictionary of individual country dataframes, and cleaning them up--pick out non-empty variables for each country.
 
 panels_dict = {}
 for p in panels.COUNTRY.unique():
@@ -72,98 +74,72 @@ for p in panels.COUNTRY.unique():
 
 
 # In[ ]:
-
+## Check that all the countries we want are there
 
 panels_dict.keys()
 
 
-# Where does the data go?
-
 # In[ ]:
-
-
+## Writing out the individual countries' panel data files
 for pais, data in panels_dict.items():
+    # You can choose to just do this for one country, e.g. for testing
     # if pais=="BR":
     print("working on "+pais)
     print(data.columns)
     if not os.path.isdir(outdir):
         os.mkdir(outdir)
-    data.columns = data.columns.str.strip()
-    data.to_csv(outdir+pais+"_netquest-panel.csv", encoding='utf8',index=False)
+    data.columns = data.columns.str.strip() # get rid of whitespace
+    data.to_csv(outdir+pais+"_netquest-panel.csv", encoding='utf8',index=False) #write out each country's panel file
 
 
 # Next, bring the variables and labels in order.
 
 # In[ ]:
 
-
+# read in the auxiliary data, skipping the header
 manual_dict = pd.read_excel(paneldir+manualfile,skiprows=1,sheet_name=None,)
+# get the variable names
 variables = manual_dict['Variables']
-variables.Variable = variables.Variable.str.strip()
+variables.Variable = variables.Variable.str.strip() # remove whitespace
 
 
 # In[ ]:
 
-
+# get well-formed table of variables, values, and labels (filling back
+# the variable labels that are just in one cell each)
 levels = pd.concat(
     [manual_dict[k] for k in manual_dict.keys() if "Código" in k]
 ).fillna(method='ffill')
 levels.columns = ["Variable","Valor","Etiqueta"]
-levels.Variable = levels.Variable.str.strip()
-#levels = pd.read_excel(paneldir+"manual_levels.xlsx")
-# this expects a the first and second sheet of netquest background export manual
-# with first line (title) removed
+levels.Variable = levels.Variable.str.strip() # remove whitespace
 
 
 # In[ ]:
 
-
+# Check that all variables have value labels
 print(("Variable" in variables.columns) & ("Variable" in levels.columns))
 
 
 # In[ ]:
 
-
-variables_dict = {}
-levels_dict = {}
-countryexp = re.compile("^([A-Z]{2,2})_")
+# Break these up by country, based on variables existing in each country
 
 for pais in panels_dict:
+    # for testing, you could just run this for one country
     # if pais=="BR":
-    all_countries = variables.Variable.apply(countryexp.findall).apply(lambda l: l[0].lower() if len(l)>0 else "None").unique()
-    other_countries = [p.lower() for p in all_countries if not p==pais]
+    
+    # we already filtered the variables that are in each country,
+    # so just use the columns of the country's data frame
     var_df = variables[
         variables.Variable.isin(panels_dict[pais].columns)
-        #remove variables that don't show up in pais
-#         [k for k in variables if k in panels_dict[pais].columns]
-#         (variables.Variable.str.lower().str.startswith(pais.lower())) #country-specific var
-#                    | (variables.Variable.apply(lambda s: sum([s.lower().startswith(k+"_") for k in all_countries]))==0) #not specific for another country
-#                    | variables.Variable.isin(["panelistAge","DESKTOP_RESPONDENT","DEVICE","DESK","TARGET",
-#                                               "SOCIODEMOGRAPHICS_DATE_V0","SOCIODEMOGRAPHICS_QUEST_V0",
-#                                               "int_municipio_delegacion"
-#                                              ])
-#                    | variables.Variable.str.contains("PNuevo")
                   ]
-
+    # pick out all the levels for the variables
     lev_df = levels[levels.Variable.isin(var_df.Variable)]
-
-    variables_dict[pais] = var_df
-    levels_dict[pais] = lev_df
-
+    
+    # write them all out    
     var_df.to_excel(outdir+pais+'_variables.xlsx',index=False)
     lev_df.to_excel(outdir+pais+'_levels.xlsx',index=False)
 
-
-# ## needs supervision
-# Check which variables are dropped at this stage
-
-# In[ ]:
-
-
-list(filter(lambda x: sum([x in v[1].Variable.unique() for v in variables_dict.items()])==0, variables.Variable))
-
-
-# In[ ]:
 
 
 
